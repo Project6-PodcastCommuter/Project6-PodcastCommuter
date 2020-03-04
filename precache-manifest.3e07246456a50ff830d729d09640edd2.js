52013998155d0ddd6b136214b9f6a97105b8fb8f
Brandon-Tang <btangsup@gmail.com>
2020-03-04 17:17:17 -0500

    Updates

---
+++ precache-manifest.3e07246456a50ff830d729d09640edd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad04ac0e8264f41a0aebda358d7059a",
+    "url": "/Project6-PodcastCommuter/index.html"
+  },
+  {
+    "revision": "8a4ab72739691adb93c6",
+    "url": "/Project6-PodcastCommuter/static/css/main.54e0a021.chunk.css"
+  },
+  {
+    "revision": "01c3700c70639532d3e5",
+    "url": "/Project6-PodcastCommuter/static/js/2.80f097c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Project6-PodcastCommuter/static/js/2.80f097c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4ab72739691adb93c6",
+    "url": "/Project6-PodcastCommuter/static/js/main.0c3d88db.chunk.js"
+  },
+  {
+    "revision": "4943563725e8502186b4",
+    "url": "/Project6-PodcastCommuter/static/js/runtime-main.546b7159.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project6-PodcastCommuter/static/media/logo.5d5d9eef.svg"
+  }
+]);
