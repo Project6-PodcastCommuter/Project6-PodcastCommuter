6eb4f87da4b5d449b93fc6a390a0c81b6631be79
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-03-05 16:50:46 -0500

    setting up podcast api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import axios from 'axios';
+
+class Map extends Component{
+  constructor(){
+    super();
+
+    this.state={
+      start:'',
+      end:'',
+    }
+  }
+
+  componentDidMount(){
+    axios({
+      url: `https://listen-api.listennotes.com/api/v2/search`,
+      method: `GET`,
+      headers: { 'X-ListenAPI-Key': 'efedd950b2d84805a5c9ede9b4543e23' },
+      dataResponse: `jsonp`,
+      params: {
+        q: 'star',
+        type: "podcast"
+      }
+    }).then((data) => {
+      console.log(data)
+    });
+  }
+
+  render(){
+    return(
+      <div>Hi</div>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
+      <Map />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
