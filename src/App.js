477c7bccc89a3cc7a526fbf563b728ee69b6189f
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-03-05 20:30:02 -0500

    static map works and formatted time shows

--- src/App.js
+++ src/App.js
@@ -3,93 +3,177 @@ import logo from './logo.svg';
 import './App.scss';
 import axios from 'axios';
 
-class Podcast extends Component{
-  constructor(){
-    super();
+// class Podcast extends Component{
+//   constructor(){
+//     super();
 
-    this.state={
-      keyword: '',
-    }
-  }
+//     this.state={
+//       keyword: '',
+//     }
+//   }
 
-  componentDidMount(){
-    axios({
-      url: `https://listen-api.listennotes.com/api/v2/search`,
-      method: `GET`,
-      headers: { 'X-ListenAPI-Key': 'efedd950b2d84805a5c9ede9b4543e23' },
-      dataResponse: `jsonp`,
-      params: {
-        q: 'star',
-        type: "podcast"
-      }
-    }).then((data) => {
-      console.log(data)
-    });
-  }
+//   componentDidMount(){
+//     axios({
+//       url: `https://listen-api.listennotes.com/api/v2/search`,
+//       method: `GET`,
+//       headers: { 'X-ListenAPI-Key': 'efedd950b2d84805a5c9ede9b4543e23' },
+//       dataResponse: `jsonp`,
+//       params: {
+//         q: 'star',
+//         type: "podcast"
+//       }
+//     }).then((data) => {
+//       console.log(data)
+//     });
+//   }
 
-  render(){
-    return(
-      <div className="podcastContent">
-        <div>Hi</div>
-        <h1>Testing</h1>
-      </div>
-    )
-  }
-}
+//   render(){
+//     return(
+//       <div className="podcastContent">
+//         <div>Hi</div>
+//         <h1>Testing</h1>
+//       </div>
+//     )
+//   }
+// }
 
-class Map extends Component{
-  constructor(){
+// class Map extends Component{
+//   constructor(){
+//     super()
+//     this.state={
+//       from: '',
+//       to: '',
+//     }
+//   }
+
+//   componentDidMount(){
+//     axios({
+//       url: 'http://www.mapquestapi.com/directions/v2/route',
+//       params: {
+//         key: 'PgwvbKwVwtViQRmH4Rju1Xri2DmysmKb',
+//         from: '5 St Joseph Street, Toronto',
+//         to: '483 Queen St W, Toronto',
+//         routeType: 'pedestrian'
+//       }
+//     }).then(response => {
+//       console.log(response)
+//     })
+//   }
+  
+//   render(){
+//     return(
+//       <div className="mapContent">
+//         <h1>Map testing</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
+class App extends Component{
+  constructor() {
     super()
-    this.state={
-      start: '',
-      end: '',
+    this.state = {
+      from: '',
+      to: '',
+      routeType: '',
+      sessionId: '',
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
+  
+  }
+
+  // onChange function
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  mapSubmit = (e) => {
+    //5 St Joseph Street, Toronto
+    //485 Queen St W, Toronto
+
+    e.preventDefault()
+    const routeType = 'pedestrian'
+
+    //axios for route
     axios({
       url: 'http://www.mapquestapi.com/directions/v2/route',
       params: {
         key: 'PgwvbKwVwtViQRmH4Rju1Xri2DmysmKb',
-        from: '5 St Joseph Street, Toronto',
-        to: '483 Queen St W, Toronto',
-        routeType: 'pedestrian'
+        from: this.state.from,
+        to: this.state.to,
+        routeType: routeType,
       }
     }).then(response => {
-      console.log(response)
+      // console.log(response)
+      // userRouteTime is a string
+      const userRouteTime = response.data.route.legs[0].formattedTime;
+      const sessionId = response.data.route.sessionId;
+
+
+      // this.setState({
+      //   sessionId: sessionId
+      // })
+      // console.log(sessionId);
+      // console.log(typeof(userRouteTime));
+      // console.log(response)
     })
-  }
-  
-  render(){
-    return(
-      <div className="mapContent">
-        <h1>Map testing</h1>
-      </div>
-    )
-  }
-}
+
+    const url = `https://www.mapquestapi.com/staticmap/v5/map?key=PgwvbKwVwtViQRmH4Rju1Xri2DmysmKb&start=${this.state.from}&end=${this.state.to}`
+    console.log(url)
 
 
-class App extends Component{
+
+
+    // axios({
+    //   url: 'https://www.mapquestapi.com/staticmap/v5/map',
+    //   params: {
+    //     key: 'PgwvbKwVwtViQRmH4Rju1Xri2DmysmKb',
+    //     start: this.state.from,
+    //     end: this.state.to,
+    //   }
+    // }).then(response => {
+    //   // console.log(response)
+    //   // userRouteTime is a string
+    //   // const userRouteTime = response.data.route.legs[0].formattedTime;
+    //   // const sessionId = response.data.route.sessionId;
+    //   console.log(response)
+    // })
+
+// Route shape for pic of map
+// Pass in longitute and latitude from previous api call to print pic of map
+// Check mapquest api for this endpoint to see the info needed
+  // axios({
+  //   url: 'http://www.mapquestapi.com/directions/v2/routeshape',
+  //     params: {
+  //       key: 'PgwvbKwVwtViQRmH4Rju1Xri2DmysmKb',
+  //       sessionId: this.state.sessionId,
+  //       fullShape: true,
+  //     }
+  //   }).then(response => {
+  //     const mapUrl = 
+  //     console.log(response)
+  //   })
+
+    
+  }
+
   render(){
     return (
       <div className="App">
-        <Podcast />
-        <Map />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BEST GROUP!🙌
-          </a>
-        </header>
+        <form action="" onSubmit={this.mapUserInput} className="mapForm">
+          <label htmlFor="from">Start</label>
+          <input type="text" id="from" name="from" value={this.state.from} onChange={this.handleChange}/>
+          <label htmlFor="end">End</label>
+          <input type="text" id="to" name="to" value={this.state.to} onChange={this.handleChange}/>
+
+          <button className="mapSubmitButton" onClick={this.mapSubmit}>Submit</button>
+        </form>
       </div>
     );
   }
