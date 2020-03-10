cf3c9af668e9db6abcb92b195f7b70f34274f392
Brandon-Tang <btangsup@gmail.com>
2020-03-10 19:22:29 -0400

    made RESPONSIVENESS FOR MOBILE TINGS

--- src/Podcast.js
+++ src/Podcast.js
@@ -31,8 +31,8 @@ class Podcast extends Component {
 
     render() {
         return (
-            <div className="podcastContent wrapper">        
-                <section>
+            <div className="podcastContent">        
+                <section className="wrapper">
                     <div>
                         <h3>Pick a Podcast</h3>
                         <p>Here are some podcasts that match your commute time.</p>
@@ -42,14 +42,14 @@ class Podcast extends Component {
                         {this.props.podData.map((response) => {
                             return (
                                 <div className="podcast">
-                                    <img src={response.podImage} alt={this.state.podTitle}></img>
+                                    <img src={response.podImage} className="podcastImg" alt={this.state.podTitle}></img>
                                     <div>
                                         <h4>{response.podTitle}</h4>
-                                        <p>{Math.floor(response.podTime / 60)} minutes</p>
+                                        <p className="podcastTime">{Math.floor(response.podTime / 60)} min.</p>
                                     </div>
                                     <p>{response.podDescription}</p>
-                                    <a>Read More</a>
-                                    <button onClick={this.selectedPodcast} value={response.podUrl}>Choose Podcast
+                                    <a className="readMore">Read More</a>
+                                    <button onClick={this.selectedPodcast} className="podcastButton" value={response.podUrl}>Choose
                                     </button>
                                 </div>
                             )
@@ -57,27 +57,28 @@ class Podcast extends Component {
                     </div>
                 </section>
                 
+                { this.state.selectedPodcast.length === 0 ? null : 
                 <section>
-                    <h3>Have a listen and enjoy your commute</h3>
-                    { this.state.selectedPodcast.length === 0 ? null : 
-                        <div className="selectedAudio">
-                            <div className="finalPodcastContent">
-                                <div className="finalPodcastImage">
-                                    <img src={this.state.selectedPodcast.podImage} alt={this.state.selectedPodcast.podTitle}></img>
-                                </div>
-                                <div className="finalPodcastInfo">
-                                    <h3>{this.state.selectedPodcast.podTitle}</h3>
-                                    <p>{this.state.selectedPodcast.podDescription}</p>
-                                </div>
+                    <h3 className="finalPodcastTag">Have a listen and enjoy your commute</h3>
+                    <div className="selectedAudio finalPodcastContent">
+                        <div>
+                            <div className="finalPodcastImage">
+                                <img src={this.state.selectedPodcast.podImage} className="finalPodcastImg" alt={this.state.selectedPodcast.podTitle}></img>
                             </div>
-                            <audio 
-                                className="finalPodcastAudio"
-                                controls
-                                src={this.state.selectedPodcast.podAudio}>
-                            </audio>
+                            <div className="finalPodcastInfo">
+                                <h3>{this.state.selectedPodcast.podTitle}</h3>
+                                <p>{this.state.selectedPodcast.podDescription}</p>
+                                <a className="readMore">Read More</a>
+                            </div>
                         </div>
-                    } 
+                        <audio 
+                            className="finalPodcastAudio"
+                            controls
+                            src={this.state.selectedPodcast.podAudio}>
+                        </audio>
+                    </div>
                 </section>
+                } 
             </div>
         )
     }
